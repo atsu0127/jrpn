5093be9438ed6ff5632f30ee6110ac85742d2b43
Atsutomo Tabata <atsu.dq9@gmail.com>
2022-01-22 01:39:39 +0900

    add askama and thiserror

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,4 +1,25 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, ResponseError};
+use thiserror::Error;
+use askama::Template;
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
+impl ResponseError for MyError {}
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
@@ -10,8 +31,21 @@ async fn main() -> Result<(), actix_web::Error> {
 }
 
 #[get("/")]
-async fn index() -> Result<impl Responder, actix_web::Error> {
-    let response_body = "Hello, World!!";
-
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index() -> Result<impl Responder, MyError> {
+    let mut entries = vec![];
+    entries.push(TodoEntry{
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry{
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(response_body)
+    )
 }
